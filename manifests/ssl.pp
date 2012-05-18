87ab2d7fb9062596d47a664764a57446bc50d72b
Benoit GUERIN <benoit.guerin1@free.fr>
2012-05-18 22:25:29 +0200

    Adding SSL subclass

---
+++ manifests/ssl.pp
@@ -0,0 +1,40 @@
+# Class apache::ssl
+#
+# Apache resources specific for SSL
+#
+class apache::ssl {
+
+  include apache
+
+  case $::operatingsystem {
+    ubuntu,debian,mint: { 
+      exec { 'enable-ssl':
+        command => '/usr/sbin/a2enmod ssl',
+        creates => '/etc/apache2/mods-enabled/ssl.load',
+        notify => Service['apache'],
+      }
+    }
+
+    default: {
+      package { 'mod_ssl':
+        ensure => present,
+        require => Package['apache'],
+        notify => Class['apache'],
+      }
+      file { "${apache::configdir}/ssl.conf":
+        mode => '0644',
+        owner => 'root',
+        group => 'root',
+        notify => Service['apache'],
+      }
+      file {['/var/cache/mod_ssl', '/var/cache/mod_ssl/scache']:
+        ensure => directory,
+        owner => 'apache',
+        group => 'root',
+        mode => '0700',
+        notify => Service['apache'],
+      }
+    }
+  }
+
+}
