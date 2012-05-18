8ad58961594d7fa6ac34f53c9413549557dc6496
Benoit GUERIN <benoit.guerin1@free.fr>
2012-05-18 22:24:46 +0200

    Fix

--- manifests/passenger.pp
+++ manifests/passenger.pp
@@ -4,13 +4,19 @@
 #
 class apache::passenger {
 
-  require apache
+  include apache
 
   case $::operatingsystem {
     ubuntu,debian,mint: { 
       package { 'libapache2-mod-passenger':
         ensure => present;
       }     
+
+      exec { 'enable-passenger':
+        command => '/usr/sbin/a2enmod passenger',
+        creates => '/etc/apache2/mods-enabled/passenger.load',
+        notify => Service['apache'],
+      }
     }
 
     centos,redhat,scientific,fedora: {
