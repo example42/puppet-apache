ea78b4f40bfa09ecbd695e668f69b3e4edf029fb
Dolf Schimmel (Freeaqingme) <dolfschimmel@gmail.com>
2013-05-05 17:11:54 +0200

    Implemented passenger support for virtual hosts

--- manifests/vhost.pp
+++ manifests/vhost.pp
@@ -25,6 +25,42 @@
 # [*serveraliaes*]
 #   An optional list of space separated ServerAliaes
 #
+# [*passenger*]
+#   If Passenger should be enabled
+#
+# [*passenger_high_performance*]
+#   Set the PassengerHighPerformance directive
+#
+# [*passenger_pool_max_pool_size*]
+#   Set the PassengerMaxPoolSize directive
+#
+# [*passenger_pool_idle_time*]
+#   Set the PassengerPoolIdleTime directive
+#
+# [*passenger_max_requests*]
+#   Set the PassengerMaxRequests directive
+#
+# [*passenger_stat_throttle_rate*]
+#   Set the PassengerStatThrottleRate directive
+#
+# [*passenger_rack_auto_detect*]
+#   Set the RackAutoDetect directive
+#
+# [*passenger_rails_auto_detect*]
+#   Set the RailsAutoDetect directive
+#
+# [*passenger_rails_env*]
+#   Set the RailsEnv directive
+#
+# [*passenger_rails_base_uri*]
+#   Set the RackBaseURI directive
+#
+# [*passenger_rack_env*]
+#   Set the RackEnv directive
+#
+# [*passenger_rack_base_uri*]
+#   Set the RackBaseURI directive
+#
 # == Example:
 #  apache::vhost { 'site.name.fqdn':
 #    docroot  => '/path/to/docroot',
@@ -36,19 +72,36 @@
 #  }
 #
 define apache::vhost (
-  $docroot        = '',
-  $docroot_create = false,
-  $docroot_owner  = 'root',
-  $docroot_group  = 'root',
-  $port           = '80',
-  $ssl            = false,
-  $template       = 'apache/virtualhost/vhost.conf.erb',
-  $priority       = '50',
-  $serveraliases  = '',
-  $enable         = true ) {
+  $docroot                       = '',
+  $docroot_create                = false,
+  $docroot_owner                 = 'root',
+  $docroot_group                 = 'root',
+  $port                          = '80',
+  $ssl                           = false,
+  $template                      = 'apache/virtualhost/vhost.conf.erb',
+  $priority                      = '50',
+  $serveraliases                 = '',
+  $enable                        = true,
+  $passenger                     = true,
+  $passenger_high_performance    = true,
+  $passenger_max_pool_size       = 12,
+  $passenger_pool_idle_time      = 1200,
+  $passenger_max_requests        = 0,
+  $passenger_stat_throttle_rate  = 30,
+  $passenger_rack_auto_detect    = true,
+  $passenger_rails_auto_detect   = false,
+  $passenger_rails_env           = undef,
+  $passenger_rails_base_uri      = '/',
+  $passenger_rack_env            = undef,
+  $passenger_rack_base_uri       = '/',
+) {
 
-  $ensure = bool2ensure($enable)
-  $bool_docroot_create = any2bool($docroot_create)
+  $ensure                            = bool2ensure($enable)
+  $bool_docroot_create               = any2bool($docroot_create)
+  $bool_passenger                    = any2bool($passenger)
+  $bool_passenger_high_performance   = any2bool($passenger_high_performance)
+  $bool_passenger_rack_auto_detect   = any2bool($passenger_rack_auto_detect)
+  $bool_passenger_rails_auto_detect  = any2bool($passenger_rails_auto_detect)
 
   $real_docroot = $docroot ? {
     ''      => "${apache::data_dir}/${name}",
@@ -94,5 +147,9 @@ define apache::vhost (
       mode    => '0775',
       require => Package['apache'],
     }
+  }
+
+  if $bool_passenger == true {
+    include apache::passenger
   }
 }
