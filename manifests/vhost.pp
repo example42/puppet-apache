e6cd3b19464882ffdb39b5acfa4e7b4e2769e3d3
Dolf Schimmel <dschimmel@transip.nl>
2013-12-18 17:19:59 +0100

    Adding SSL capabilities to default vhost

--- manifests/vhost.pp
+++ manifests/vhost.pp
@@ -122,6 +122,10 @@ define apache::vhost (
   $docroot_group                = 'root',
   $port                         = '80',
   $ssl                          = false,
+  $ssl_crt_file                 = '',
+  $ssl_cert_key_file            = '',
+  $ssl_crt_chain_file           = '',
+  $ssl_cipher_suite             = 'HIGH:!aNULL:!MD5',
   $template                     = 'apache/virtualhost/vhost.conf.erb',
   $source                       = '',
   $priority                     = '50',
@@ -156,6 +160,7 @@ define apache::vhost (
   $bool_passenger_high_performance   = any2bool($passenger_high_performance)
   $bool_passenger_rack_auto_detect   = any2bool($passenger_rack_auto_detect)
   $bool_passenger_rails_auto_detect  = any2bool($passenger_rails_auto_detect)
+  $bool_ssl                          = any2bool($ssl)
 
   $real_docroot = $docroot ? {
     ''      => "${apache::data_dir}/${name}",
@@ -211,6 +216,12 @@ define apache::vhost (
   }
 
   include apache
+
+  if $bool_ssl {
+    if ! defined(Apache::Module['ssl']) {
+      apache::module { 'ssl': }
+    }
+  }
 
   file { $config_file_path:
     ensure  => $ensure,
