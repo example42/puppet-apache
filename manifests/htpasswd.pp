fcc8167c196c359faf03ef3adf7131c9e13d4883
Israel Borges <israelborges@zertico.com>
2015-01-23 15:04:48 -0200

    I cant add the user same in diferent files This problem was described in an issue: "apache::htpasswd uses the name of the resource as the username to be added to the htpasswd file, and does not provide a way to override that username. That causes a problem when you want to have the same username defined in two different htpasswd files on the same node. If I try to create entries for user1 in both /etc/htpasswd1 and /etc/htpasswd2, I'll get a resource conflict. A simple solution to this would be to add an additional parameter to apache::htpasswd called username that defaults to $name."

--- manifests/htpasswd.pp
+++ manifests/htpasswd.pp
@@ -7,7 +7,7 @@
 # == Parameters
 #
 # [*ensure*]
-#   Define if the add (present) or remove the user (set as $name)
+#   Define if the add (present) or remove the user (set as $username)
 #   Default: 'present',
 #
 # [*htpasswd_file*]
@@ -36,9 +36,23 @@
 #   htpasswd_file  => '/etc/httpd/users.passwd',
 # }
 #
+# Set the same user in diferent files 
+# apache::htpasswd { 'myuser':
+#   crypt_password => 'password1',
+#   username       => ''
+#   htpasswd_file  => '/etc/httpd/users.passwd',
+# }
+#
+# apache::htpasswd { 'myuser2':
+#   crypt_password => 'password2',
+#   username       => 'myuser'
+#   htpasswd_file  => '/etc/httpd/httpd',
+# }
+#
 define apache::htpasswd (
   $ensure           = 'present',
   $htpasswd_file    = '',
+  $username         = $name,
   $crypt_password   = false,
   $clear_password   = false ) {
 
@@ -61,28 +75,28 @@ define apache::htpasswd (
       }
 
       if $crypt_password {
-        exec { "test -f ${real_htpasswd_file} || OPT='-c'; htpasswd -bp \${OPT} ${real_htpasswd_file} ${name} '${crypt_password}'":
-          unless  => "grep -q '${name}:${crypt_password}' ${real_htpasswd_file}",
+        exec { "test -f ${real_htpasswd_file} || OPT='-c'; htpasswd -bp \${OPT} ${real_htpasswd_file} ${username} '${crypt_password}'":
+          unless  => "grep -q '${username}:${crypt_password}' ${real_htpasswd_file}",
           path    => '/bin:/sbin:/usr/bin:/usr/sbin',
         }
       }
 
       if $clear_password {
-        exec { "test -f ${real_htpasswd_file} || OPT='-c'; htpasswd -b \$OPT ${real_htpasswd_file} ${name} ${clear_password}":
-          unless  => "egrep '^${name}:' ${real_htpasswd_file} && grep ${name}:\$(mkpasswd -S \$(egrep '^${name}:' ${real_htpasswd_file} |cut -d : -f 2 |cut -c-2) ${clear_password}) ${real_htpasswd_file}",
+        exec { "test -f ${real_htpasswd_file} || OPT='-c'; htpasswd -b \$OPT ${real_htpasswd_file} ${username} ${clear_password}":
+          unless  => "egrep '^${username}:' ${real_htpasswd_file} && grep ${username}:\$(mkpasswd -S \$(egrep '^${username}:' ${real_htpasswd_file} |cut -d : -f 2 |cut -c-2) ${clear_password}) ${real_htpasswd_file}",
           path    => '/bin:/sbin:/usr/bin:/usr/sbin',
         }
       }
     }
 
     'absent': {
-      exec { "htpasswd -D ${real_htpasswd_file} ${name}":
-        onlyif => "egrep -q '^${name}:' ${real_htpasswd_file}",
-        notify => Exec["delete ${real_htpasswd_file} after remove ${name}"],
+      exec { "htpasswd -D ${real_htpasswd_file} ${username}":
+        onlyif => "egrep -q '^${username}:' ${real_htpasswd_file}",
+        notify => Exec["delete ${real_htpasswd_file} after remove ${username}"],
         path   => '/bin:/sbin:/usr/bin:/usr/sbin',
       }
 
-      exec { "delete ${real_htpasswd_file} after remove ${name}":
+      exec { "delete ${real_htpasswd_file} after remove ${username}":
         command     => "rm -f ${real_htpasswd_file}",
         onlyif      => "wc -l ${real_htpasswd_file} | egrep -q '^0[^0-9]'",
         refreshonly => true,
